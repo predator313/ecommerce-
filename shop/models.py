8ee42d569b3233495eb31ec52c64cd029895ca22
Aamir Ashraf <aamirnasa97@gmail.com>
2022-11-22 02:57:51 +0530

    making product db

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id=models.AutoField #like primary key
+    product_name=models.CharField(max_length=30)
+    desc=models.CharField(max_length=200)
+    published_date=models.DateField()
